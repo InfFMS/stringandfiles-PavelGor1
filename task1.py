266e241ff252babaa09b7d5dfceeb50ee15eba90
PavelGor1 <116425791+PavelGor1@users.noreply.github.com>
2025-01-20 22:43:29 +0700

    Update task1.py

--- task1.py
+++ task1.py
@@ -6,14 +6,16 @@
 # Выведите полученную статистику на экран.
 file =open('task1.txt', encoding ='utf8')
 lines = 0
-words = 0
 simvols = 0
 for line in file:
     lines += 1
-    words += len(line.split())
     simvols += len(line)
-if words == '-':
-    words -=1
+file.close()
+file = open('task1.txt',encoding='utf8')
+a= file.read()
+a = a.split("—")
+word = " ".join(a)
+words = len(word.split())
 print(' Общее количество строк в файле:',lines)
 print('Общее количество слов во всем тексте файла.',words)
 print('Общее количество символов',simvols)
